975d4e31391ac349af4229752a12558f9eb82baa
MANJURUL13673 <manjurulhaque552@gmail.com>
2021-02-02 00:35:26 +0600

    boolean and null value

---
+++ 01Basics/nullandBool.js
@@ -0,0 +1,19 @@
+//undefined
+var temp;
+
+console.log('Current temperature is: ' + temp);
+
+//Grade system
+
+// 10 - Great job
+// 5 - work harder
+// 2 - poor
+// 0 - fail
+
+//Boolean - True/False
+
+let actualMarks = 7;
+
+let myGrade = (actualMarks < 10);
+
+console.log(myGrade);
