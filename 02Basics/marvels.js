6ebca019a522c8a3df7b4b14d8fe3d91a1bf3e99
MANJURUL13673 <manjurulhaque552@gmail.com>
2021-02-02 21:16:41 +0600

    shift unshift push pop and splice method in Node JS

--- 02Basics/marvels.js
+++ 02Basics/marvels.js
@@ -1,11 +1,40 @@
 //datatype - Array
 
-const superHeroes = ['Iron Man', 'Spiderman', 'Capt. America', 'Thor'];
+// const superHeroes = ['Iron Man', 'Spiderman', 'Capt. America', 'Thor'];
 
-console.log(superHeroes);
-console.log(superHeroes[0]);
-console.log(superHeroes[2]);
-console.log(superHeroes.length);
-console.log(superHeroes[superHeroes.length - 1]);
+// console.log(superHeroes);
+// console.log(superHeroes[0]);
+// console.log(superHeroes[2]);
+// console.log(superHeroes.length);
+// console.log(superHeroes[superHeroes.length - 1]);
 
-console.log(`We have ${superHeroes.length} super heroes`);
+// console.log(`We have ${superHeroes.length} super heroes`);
+
+const numbers = ['One', 'Two', 'Three', 'Four', 'Five', 'Six'];
+
+numbers[1] = 'SOMETHING'; //replace value at a position
+
+console.log(numbers);
+
+//start 
+
+// numbers.shift();    //deleted first element
+// console.log(numbers);
+// console.log(numbers.shift());    //print the deleted element
+
+// numbers.unshift('SOMETHING');  //Insert first element
+// console.log(numbers);
+
+//end
+
+// numbers.pop();  //delete the last element
+// console.log(numbers);
+// console.log(numbers.pop())  //show the delete item;
+
+numbers.push('Seven');
+console.log(numbers);
+
+//middle
+
+numbers.splice(2, 2, 'SOMETHING');  //take numbers of item and insert value in that place
+console.log(numbers);
