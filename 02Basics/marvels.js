e0a306c96e0ef810bcd5726de7eed87bd20e47a3
MANJURUL13673 <manjurulhaque552@gmail.com>
2021-02-02 21:03:40 +0600

    Array in Node JS

---
+++ 02Basics/marvels.js
@@ -0,0 +1,11 @@
+//datatype - Array
+
+const superHeroes = ['Iron Man', 'Spiderman', 'Capt. America', 'Thor'];
+
+console.log(superHeroes);
+console.log(superHeroes[0]);
+console.log(superHeroes[2]);
+console.log(superHeroes.length);
+console.log(superHeroes[superHeroes.length - 1]);
+
+console.log(`We have ${superHeroes.length} super heroes`);
