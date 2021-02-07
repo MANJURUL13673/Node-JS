25e11fd5341df5615f33a432c049a8736e322056
MANJURUL13673 <manjurulhaque552@gmail.com>
2021-02-07 22:53:20 +0600

    two equals and three equals sign in Node JS

---
+++ 02Intermediate/equals.js
@@ -0,0 +1,23 @@
+console.log('' === '');
+
+let myVar = '';
+let myVarTwo = '';
+
+console.log(myVar == myVarTwo);
+console.log(myVar === myVarTwo);
+
+let myObj = {};
+let myObjTwo = {};
+
+console.log(myObj == myObjTwo);
+console.log(myObj === myObjTwo);
+
+let myObj2 = {};
+let myObjTwo2 = myObj2;
+
+console.log(myObj2 == myObjTwo2);
+console.log(myObj2 === myObjTwo2);
+
+console.log(2 == "2");
+console.log(2 === "2");
+
