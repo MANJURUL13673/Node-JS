b8f47b5ab4b1424a3056acf11b7fafeb06a60b2d
MANJURUL13673 <manjurulhaque552@gmail.com>
2021-02-07 21:26:54 +0600

    Review array and loop in Node JS

--- 01Intermediate/trello.js
+++ 01Intermediate/trello.js
@@ -1,24 +1,41 @@
-const days = ['Mon', 'Tue', 'Wed', 'Thr', 'Fri', 'Sat'];
+// const days = ['Mon', 'Tue', 'Wed', 'Thr', 'Fri', 'Sat'];
 
-console.log(days[0]);
+// console.log(days[0]);
 
-days.forEach(function (day, index) {
-    console.log(`starts with ${index+1} -- ${day}`);
+// days.forEach(function (day, index) {
+//     console.log(`starts with ${index+1} -- ${day}`);
+// });
+
+// //Assignment
+
+// const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+// months.forEach(function (month, index) {
+//     console.log(`starts with ${index+1} -- ${month}`);
+// });
+
+// for (let i = 0; i < days.length; i++) {
+//     console.log(`starts with ${i+1} -- ${days[i]}`);
+// }
+
+// //reverse print
+// for(let i = days.length; i >= 0; i--) {
+//     console.log(`starts with ${i+1} -- ${days[i]}`);
+// }
+
+const myTodos = []
+
+myTodos.push('Buy Bread');
+myTodos.push('Record Videos for youtube');
+myTodos.push('Got to Gym');
+
+myTodos.forEach(function (todo, index) {
+    console.log(`Your task no. ${index + 1} is: ${todo}`);
 });
 
 //Assignment
+myTodos.unshift('Morning prayer');
 
-const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-
-months.forEach(function (month, index) {
-    console.log(`starts with ${index+1} -- ${month}`);
-});
-
-for (let i = 0; i < days.length; i++) {
-    console.log(`starts with ${i+1} -- ${days[i]}`);
-}
-
-//reverse print
-for(let i = days.length; i >= 0; i--) {
-    console.log(`starts with ${i+1} -- ${days[i]}`);
+for(let i = 0; i < myTodos.length; i++) {
+    console.log(`Your task no. ${i + 1} is: ${myTodos[i]}`);
 }
