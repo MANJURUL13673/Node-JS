35ac328e3c29e67d506e1f0dc3f6f5d2c9c22eb9
MANJURUL13673 <manjurulhaque552@gmail.com>
2021-02-08 21:50:10 +0600

    Objects with Funtions in Node JS

--- 02Intermediate/myObjects.js
+++ 02Intermediate/myObjects.js
@@ -17,3 +17,32 @@ let myCourse = {
 }
 
 console.log(`Hey the course ${myCourse.name} author by ${myCourse.author} and price ${myCourse.price}`);
+
+// myYoutubeVideo.title = 'ForEach loop in JS';
+
+// console.log(`Hey new Video on title by ${myYoutubeVideo.title} by author ${myYoutubeVideo.videoCreator}`);
+// console.log(myYoutubeVideo);
+
+let myYoutubeVideoOne = {
+    title: 'Loops in javascript',
+    videoLength: 15,
+    videoCreator: 'Manjurul Haque',
+    videoDescription: 'this is a video description and a long one'
+}
+
+let myYoutubeVideoTwo = {
+    title: 'Loops in javascript',
+    videoLength: 10,
+    videoCreator: 'Manjurul Haque',
+    videoDescription: 'this is a video description and a long one'
+}
+
+let changeVideoLength = function(myObject) {
+    return {
+        formatOne: `Time of this video is: ${myObject.videoLength + 2}`,
+        formatTwo: `Time of this video is: ${myObject.videoLength + 2}`
+    }
+}
+
+let adOne = changeVideoLength(myYoutubeVideoTwo);
+console.log(adOne.formatOne);
