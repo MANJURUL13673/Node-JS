b572e8e89bc9cfd8ed0e40738da95c6cb49fd38a
MANJURUL13673 <manjurulhaque552@gmail.com>
2021-02-08 19:56:01 +0600

    Objects in Node JS

---
+++ 02Intermediate/myObjects.js
@@ -0,0 +1,19 @@
+let myYoutubeVideo = {
+    title: 'Loops in javascript',
+    videoLength: 15,
+    videoCreator: 'Manjurul Haque',
+    videoDescription: 'this is a video description and a long one'
+}
+
+console.log(myYoutubeVideo);
+
+console.log(`Hey new Video on title by ${myYoutubeVideo.title} by author ${myYoutubeVideo.videoCreator}`);
+
+//Assignment
+let myCourse = {
+    name: 'JavaScript',
+    author: 'Manjurul Haque',
+    price: 50
+}
+
+console.log(`Hey the course ${myCourse.name} author by ${myCourse.author} and price ${myCourse.price}`);
