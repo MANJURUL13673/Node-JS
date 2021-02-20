5a7cc9ae30afb2538d275ae76deec7418d58a9b4
MANJURUL13673 <manjurulhaque552@gmail.com>
2021-02-20 16:35:07 +0600

    Coercion in Node JS

---
+++ 01Advance/coercion.js
@@ -0,0 +1,28 @@
+console.log('5' + 5);
+
+//const giveType = typeof 5
+
+//console.log(giveType);
+
+const adder = false + 5;
+
+console.log(adder);
+
+
+const loginDetails = []
+//login for getting details from DB
+
+const loginID = loginDetails[0]
+
+if (loginID) {
+    console.log('Allow user to login');
+} else {
+    console.log('Auth failed');
+}
+
+// Values that interpret as false:
+//false
+//0
+//''
+//null
+//undefined
