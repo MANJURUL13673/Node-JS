31d4396744a842fffcdaca989fa659f5b32d9038
MANJURUL13673 <manjurulhaque552@gmail.com>
2021-02-21 00:04:18 +0600

    change element through JS

--- 01WebBasics/myscript.js
+++ 01WebBasics/myscript.js
@@ -1,3 +1,17 @@
-const myElement = document.querySelectorAll('.classone');
+// const myElement = document.querySelectorAll('.classone');
 
-console.log(myElement[0]);
+// console.log(myElement[0]);
+
+// const myPElement = document.querySelector('p');
+// myPElement.textContent = 'I am being changed using JS';
+
+const myPElement = document.querySelectorAll('p');
+
+// myPElement.forEach(function(p){
+//     p.textContent = 'I am changed by JS loop'
+// })
+
+//Assignment using Arrow Function
+myPElement.forEach((p) => {
+    p.textContent = 'I am changed using Arrow function of JS'
+});
