89ed8ab967b08be9f4d0ed56868ebdcd3988b928
MANJURUL13673 <manjurulhaque552@gmail.com>
2021-02-20 12:41:10 +0600

    Arrow Function in Node JS

---
+++ 01Advance/arrow.js
@@ -0,0 +1,48 @@
+// const sayHello = function(name){
+//     return "Hey there, " + name + " !";
+// }
+
+// console.log(sayHello("Sammy"));
+
+// const sayHello = (name) => `hey there ${name} !`
+
+
+// console.log(sayHello("Sammy"));
+
+
+// const todos = [{
+//     title: 'Buy bread',
+//     isDone: true
+// }, {
+//     title: 'Go to gym',
+//     isDone: true
+// }, {
+//     title: 'Record youtube video',
+//     isDone: false
+// }]
+
+// const thingsDone = todos.filter((todo) => todo.isDone === true )
+// console.log(thingsDone);
+
+//Assignment
+const todos = [{
+    title: 'praying salah',
+    isDone: true
+}, {
+    title: 'office work',
+    isDone: false
+}, {
+    title: 'problem solving',
+    isDone: false
+}, {
+    title: 'youtube video',
+    isDone: false
+}, {
+    title: 'learn node js',
+    isDone: true
+}]
+
+//const things = todos.keys((todo) => todo.isDone === false);
+const todosNotDone = (todos.filter((todo) => todo.isDone === false)).map((todo) => todo.title);
+
+console.log(todosNotDone);
