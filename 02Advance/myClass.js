bb849eb83f81327c1739b431126f6bf7a4a85c10
MANJURUL13673 <manjurulhaque552@gmail.com>
2021-02-28 11:24:10 +0600

    Inheritence in JS

--- 02Advance/myClass.js
+++ 02Advance/myClass.js
@@ -24,10 +24,23 @@ class User{
     }
 }
 
-const john = new User('John', 'Anderson', 34);
+class Teacher extends User {
+    constructor(firstname, lastname, credit, subject) {
+        super(firstname, lastname, credit, subject);
+        this.subject = subject;
+    }
+    getFullName() {
+        return this.firstname + ' ' + this.middlename + ' ' + this.lastname + ' and subject is ' + this.subject;
+    }
+}
+
+//create object
+const john = new Teacher('John', 'Anderson', 34, 'Python');
 console.log(john.getFullName());
+//edit the name
 john.editName('Johnny Anderson');
 console.log(john.getFullName());
+//take middle name
 john.setMiddleName('Doe');
 console.log(john.getFullName());
 // const sammy = new User();
