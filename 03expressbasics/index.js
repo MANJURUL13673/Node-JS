0baa604f4f7cf09dac440aa67137b66016585b03
MANJURUL13673 <manjurulhaque552@gmail.com>
2021-03-04 23:11:03 +0600

    Routing in express

--- 03expressbasics/index.js
+++ 03expressbasics/index.js
@@ -9,15 +9,25 @@ app.get('/about',(req, res) => {
     res.send('<h1>I am about page<h1>');
 });
 
-//assignment
-//create route for contact us and services 
-app.get('/contact',(req, res) => {
-    res.send('I am in contact');
-})
+app.post('/login',(req, res) => {
+    res.send('login success');
+});
 
-app.get('/services',(req, res) => {
-    res.send('<ul><li>Video Editing</li><li>Vloging</li><li>Laundry</li></ul>');
-})
+//assignment 1
+//create route for contact us and services 
+// app.get('/contact',(req, res) => {
+//     res.send('I am in contact');
+// })
+
+// app.get('/services',(req, res) => {
+//     res.send('<ul><li>Video Editing</li><li>Vloging</li><li>Laundry</li></ul>');
+// })
+
+//assignment 2
+//create a delete route and test it using postman
+app.post('/delete',(req, res) => {
+    res.send('delete success');
+});
 
 app.listen(3000, () => {
     console.log('Server is running at port 3000....');
