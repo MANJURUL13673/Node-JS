1bc619a2b0169b194490dd8523569137cc439b33
MANJURUL13673 <manjurulhaque552@gmail.com>
2021-03-07 00:35:58 +0600

    Server static file and form data

--- 05bodyparser/index.js
+++ 05bodyparser/index.js
@@ -1,3 +1,21 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 
+let app = express();
+
+app.use(express.urlencoded({extended: false}));
+
+app.use('/login',express.static(__dirname + "/public"));
+
+app.get('/', (req, res) => {
+    res.send('Hello, my application!');
+});
+
+app.post('/login', (req, res) => {
+    console.log(req.body.email);
+    //do some database process
+    res.redirect('/');
+})
+
+app.listen(3000, () => {
+    console.log('Server is running at port 3000');
+})
